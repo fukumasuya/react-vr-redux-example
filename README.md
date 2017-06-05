080d57b99888aaf5f1fb107a84fa493956928706
fukumasuya <fukumasuya@gmail.com>
2017-06-05 22:57:58 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+React VR + Redux example
+====
+
+This is a React VR + Redux simple example. You can see several buttons in the VR UI and the background image is changed when clicking on the buttons.
+
+## Description
+React VR is a tool to create Web VR apps.
+You can use React VR in the same manner as React. Many React applications choose Redux to manage application states. You can also use Redux with React VR in the same manner. This example uses Redux as the single store of the app to manage application states.
+
+All you need to see is in index.vr.js. Reducer, Action Creator, Store, React VR components, Redux Container are in the file.
+
+This project is created by using [react-vr](https://github.com/facebook/react-vr) CLI. So you can see the detail information of the example by checking the project.
+
+## Screen shots
+
+
+## Requirement
+- Node.js version 6.0.0 or higher
+- `yarn` or `npm` (>= v3.0.0) package managers
+
+## Install
+```bash
+$ npm i
+```
+or
+```bash
+$ yarn
+```
+
+## Usage
+```bash
+$ npm run start
+```
+or
+```bash
+$ yarn start
+```
+then, open http://localhost:8081/vr
+
+## Licence
+MIT
---
+++ index.vr.js
@@ -0,0 +1,139 @@
+import React from "react";
+import { createStore } from "redux";
+import { connect, Provider } from "react-redux";
+
+import { AppRegistry, Pano, Text, View, VrButton } from "react-vr";
+
+const uriTemplate = "https://source.unsplash.com/1600x900/?";
+const defaultData = {
+  text: "Tokyo",
+  keyword: "tokyo"
+};
+
+// Reducer
+const reducer = (state = defaultData, action) => {
+  switch (action.type) {
+    case "UPDATE":
+      return {
+        text: action.payload.text,
+        keyword: action.payload.keyword
+      };
+    default:
+      return state;
+  }
+};
+
+// Redux store
+const store = createStore(reducer);
+
+// Action Creator
+const update = data => ({
+  type: "UPDATE",
+  payload: data
+});
+
+const mapStateToProps = state => ({
+  text: state.text,
+  keyword: state.keyword
+});
+
+const mapDispatchToProps = dispatch => ({
+  onClick: keyword => dispatch(update(keyword))
+});
+
+// Component styles
+const styles = {
+  textArea: {
+    marginBottom: 0.15
+  },
+  text: {
+    backgroundColor: "transparent",
+    fontSize: 0.5,
+    fontWeight: "400",
+    layoutOrigin: [0.5, 0.5],
+    textAlign: "center",
+    textAlignVertical: "center",
+    transform: [{ translate: [0, 0, -3] }]
+  },
+  buttonArea: {
+    flexDirection: "row",
+    layoutOrigin: [0.5, 0.5],
+    marginLeft: 0.3,
+    marginRight: 0.3
+  },
+  button: {
+    width: 0.8
+  },
+  buttonText: {
+    backgroundColor: "black",
+    height: 0.3,
+    color: "yellowgreen",
+    fontSize: 0.12,
+    fontWeight: "400",
+    layoutOrigin: [0.5, 0.5],
+    paddingLeft: 0.1,
+    paddingRight: 0.1,
+    marginLeft: 0.1,
+    marginRight: 0.1,
+    textAlign: "center",
+    textAlignVertical: "center",
+    transform: [{ translate: [0, 0, -3] }],
+    borderRadius: 0.1
+  }
+};
+
+const buttonData = [
+  {
+    text: "Tokyo",
+    keyword: "tokyo"
+  },
+  {
+    text: "Kyoto",
+    keyword: "Kyoto"
+  },
+  {
+    text: "New York",
+    keyword: "new york"
+  },
+  {
+    text: "Paris",
+    keyword: "paris"
+  }
+];
+
+// VR components
+const ButtonList = ({ buttonData, onClick }) =>
+  buttonData.map((data, index) =>
+    <VrButton
+      key={`button_${index}`}
+      style={styles.button}
+      onClick={() => onClick(data)}
+    >
+      <Text style={styles.buttonText}>
+        {data.text}
+      </Text>
+    </VrButton>
+  );
+
+const SampleVR = ({ text, keyword, onClick }) =>
+  <View>
+    <Pano source={{ uri: `${uriTemplate}${keyword}` }} />
+    <View style={styles.textArea}>
+      <Text style={styles.text}>
+        {text}
+      </Text>
+    </View>
+    <View style={styles.buttonArea}>
+      {ButtonList({ buttonData, onClick })}
+    </View>
+  </View>;
+
+// Redux container
+const Container = connect(mapStateToProps, mapDispatchToProps)(SampleVR);
+
+const App = () =>
+  <Provider store={store}>
+    <Container />
+  </Provider>;
+
+AppRegistry.registerComponent("app", () => App);
